d03be73048a3a0e2b2798daa6045c1a57b44a5bf
SO <omarbagaev@gmail.com>
2019-08-10 22:42:00 +1200

    - set background in main.js;

--- dist/js/main.js
+++ dist/js/main.js
@@ -53,3 +53,58 @@ function setBgGreet() {
         document.body.style.color = 'white'
     }
 }
+
+// Get Name
+function getName() {
+    if (localStorage.getItem('name') === null) {
+        name.textContent = '[Enter Name]'
+    } else {
+        name.textContent = localStorage.getItem('name')
+    }
+}
+
+// Set Name
+function setName(e) {
+    if (e.type === 'keypress') {
+        // Make sure enter is pressed
+        if (e.which == 13 || e.keyCode == 13) {
+            localStorage.setItem('name', e.target.innerText)
+            name.blur()
+        }
+    } else {
+        localStorage.setItem('name', e.target.innerText)
+    }
+}
+
+// Get Focus
+function getFocus() {
+    if (localStorage.getItem('focus') === null) {
+        focus.textContent = '[Enter Focus]'
+    } else {
+        focus.textContent = localStorage.getItem('focus')
+    }
+}
+
+// Set Focus
+function setFocus(e) {
+    if (e.type === 'keypress') {
+        // Make sure enter is pressed
+        if (e.which == 13 || e.keyCode == 13) {
+            localStorage.setItem('focus', e.target.innerText)
+            focus.blur()
+        }
+    } else {
+        localStorage.setItem('focus', e.target.innerText)
+    }
+}
+
+name.addEventListener('keypress', setName)
+name.addEventListener('blur', setName)
+focus.addEventListener('keypress', setFocus)
+focus.addEventListener('blur', setFocus)
+
+// Run
+showTime()
+setBgGreet()
+getName()
+getFocus()
